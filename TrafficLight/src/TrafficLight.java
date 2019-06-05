0345628498d0209c398c69fe090eca8cf7a04c2f
sergannn <su.vasiliev@gmail.com>
2019-06-05 19:12:23 +0300

    Add files via upload

---
+++ TrafficLight/src/TrafficLight.java
@@ -0,0 +1,21 @@
+// Template for Lab P02 Excercise 1
+class TrafficLight {
+    private boolean red;
+
+    public TrafficLight() {
+        red = true;
+    }
+
+    public synchronized void passby() {
+        // ToDo: wait as long the light is red
+    }
+
+    public synchronized void switchToRed() {
+        // ToDo: set light to red
+    }
+
+    public synchronized void switchToGreen() {
+        // Todo: set light to green
+        // waiting cars can now pass by
+    }
+}
